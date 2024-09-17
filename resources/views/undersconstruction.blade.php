d76f7420eb4df97bf71ce8041037398929d4bc54
“[John <[rjohnquia@gmail.com]>
2024-09-17 09:44:01 +0800

    underconstruction notice

---
+++ resources/views/undersconstruction.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CPNC</title>
+</head>
+
+<body>
+    <h1>This site is under construction</h1>
+    <a href="http://vnbc.edu.ph">Visit our site here!</a>
+</body>
+
+</html>
