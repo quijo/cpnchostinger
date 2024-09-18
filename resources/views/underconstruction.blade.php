a907860842a49f14507252683ab12c66a5172609
“[John <[rjohnquia@gmail.com]>
2024-09-18 14:01:26 +0800

    debug

--- resources/views/underconstruction.blade.php
+++ resources/views/underconstruction.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CPNC</title>
-    <link rel="stylesheet" href="{{ @vite('resources/css/app.css') }}">
+    {{-- <link rel="stylesheet" href="{{ @vite('resources/css/app.css') }}"> --}}
     @vite('resources/css/app.css')
 </head>
 
