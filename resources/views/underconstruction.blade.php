513605bc23edfe6dc1e5ad9739891e2fdf966f9c
“[John <[rjohnquia@gmail.com]>
2024-09-18 13:46:24 +0800

    test tailwind

--- resources/views/underconstruction.blade.php
+++ resources/views/underconstruction.blade.php
@@ -5,27 +5,21 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CPNC</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-    <link rel="stylesheet" href="{{ asset('css/style2.css') }}">
-
+    @vite('resources/css/app.css')
 </head>
 
 <body>
-    <div class="coming">
+    {{-- <div class="coming">
         <h1>Coming Soooooon!</h1>
         <a href="http://vnbc.edu.ph">Please use this site</a>
-    </div>
+    </div> --}}
 
-    <div class="test">
+    <div class="text-3xl font-bold underline">
         hello
     </div>
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-    </script>
+
 </body>
 
 </html>
