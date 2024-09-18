279b710871506a6ea2db518f3984b09ce9bca8f7
“[John <[rjohnquia@gmail.com]>
2024-09-18 15:52:45 +0800

    test tailwind

--- resources/views/underconstruction.blade.php
+++ resources/views/underconstruction.blade.php
@@ -6,15 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CPNC</title>
     <link rel="stylesheet" href="{{ asset('css/style2.css') }}">
-
+    <script src="https://cdn.tailwindcss.com"></script>
 
 
 </head>
 
 <body>
-    <div class="coming">
-        <h1>Coming Soon!</h1>
-        <a href="https://vnbc.edu.ph">vnbc.edu.ph</a>
+    <div class="coming gap-5">
+        <h1 class="text-5xl ">Coming Soon!</h1>
+        <a class="text-2xl" href="https://vnbc.edu.ph">vnbc.edu.ph</a>
     </div>
 
 
