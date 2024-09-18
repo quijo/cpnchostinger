a0d6d7730d582f49e5a611554d094c48a2c570a3
“[John <[rjohnquia@gmail.com]>
2024-09-18 14:52:10 +0800

    css

--- resources/views/underconstruction.blade.php
+++ resources/views/underconstruction.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CPNC</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/style2.css') }}">
 
 
 
