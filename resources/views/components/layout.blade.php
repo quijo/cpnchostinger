2d4de8dfb498866d3dba82f3dcf84d03fb7061ce
“[John <[rjohnquia@gmail.com]>
2024-09-26 13:56:12 +0800

    add chat plugins

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -18,6 +18,21 @@
     {{ $slot }}
 
     <script src="{{ asset('js/app.js') }}"></script>
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API = Tawk_API || {},
+            Tawk_LoadStart = new Date();
+        (function() {
+            var s1 = document.createElement("script"),
+                s0 = document.getElementsByTagName("script")[0];
+            s1.async = true;
+            s1.src = 'https://embed.tawk.to/66f4f070e5982d6c7bb49abd/1i8mcmej7';
+            s1.charset = 'UTF-8';
+            s1.setAttribute('crossorigin', '*');
+            s0.parentNode.insertBefore(s1, s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
 </body>
 
 </html>
