c4df2f06414656704e8a5433e8bdcff6a86d7dbd
John Quiachon <rjohnquia@gmail.com>
2026-04-24 10:36:09 +0800

    edit email

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -18,7 +18,7 @@ class ContactController extends Controller
         Mail::raw(
             "Name: {$request->name}\nEmail: {$request->email}\n\nMessage:\n{$request->message}",
             function ($mail) {
-                $mail->to('rdquiachon@cpnc.edu.ph')
+                $mail->to('registrar@cpnc.edu.ph')
                      ->subject('New Message from the Website');
             }
         );
