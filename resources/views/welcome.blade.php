175196154c856eaa3cf127ca262190cf8c40f67b
John Quiachon <rjohnquia@gmail.com>
2026-04-24 09:56:06 +0800

    fix contact form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -539,7 +539,8 @@ function staffComponent() {
             {id: 27, name: 'Larry D. Babao', role: ' High School Coordinator / AP Teacher', category: 'basiced', img: '/images/temporary.jpg'},
             {id: 28, name: 'John Quiachon', role: ' IT - Coordinator / Computer Teacher', category: 'staff', img: '/images/temporary.jpg'},
             {id: 29, name: 'Raquel D. Quiachon', role: ' Computer Teacher', category: 'basiced', img: '/images/basiced/raquel.jfif'},
-            {id: 30, name: 'Josefin M. Anoos', role: 'College Faculty', category: 'college', img: '/images/temporary.jpg'},
+            {id: 30, name: 'Josefin M. Anoos', role: 'College Faculty', category: 'college', img: '/images/adco/jo.jfif'},
+            {id: 30, name: 'Rovelyn Villotes', role: 'Pre-Kinder Adviser', category: 'basiced', img: '/images/temporary.jpg},
            
         ],
 
